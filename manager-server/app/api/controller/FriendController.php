7667ef64403be3db2e120462f30713470fa64dd0
Huaqiwill <3173484026@qq.com>
2024-10-23 09:27:44 +0800

    更新档案管理，进行容错处理

--- manager-server/app/api/controller/FriendController.php
+++ manager-server/app/api/controller/FriendController.php
@@ -3,6 +3,7 @@
 namespace app\api\controller;
 
 use app\BaseController;
+use Exception;
 use think\facade\Db;
 
 /**
@@ -11,57 +12,91 @@ use think\facade\Db;
  */
 class FriendController extends BaseController
 {
-    public function getParam($name, $default = '')
-    {
-        return $this->request->param($name, $default);
-    }
-
+    /**
+     * 用户档案新增
+     * @return \think\response\Json
+     */
     public function create()
     {
-        $data = [
-            'name' => $this->getParam('name'),
-            'sex' => $this->getParam('sex'),
-            'birth_date' => $this->getParam('birth_date'),
-            'status' => $this->getParam('status'),
-            'avatar' => $this->getParam('avatar'),
-            'phone' => $this->getParam('phone'),
-            'qq' => $this->getParam('qq'),
-            'wechat' => $this->getParam('wechat'),
-            'email' => $this->getParam('email'),
-            'address' => $this->getParam('address'),
-            'remark' => $this->getParam('remark'),
-        ];
-        $id =  Db::table('app_friend')->insert($data, true);
-        $data['id'] = $id;
-        return $this->jsonResponse($data);
+        try {
+            $data = [
+                'name' => $this->getParam('name'),
+                'sex' => $this->getParam('sex'),
+                'birth_date' => $this->getParam('birth_date'),
+                'status' => $this->getParam('status'),
+                'avatar' => $this->getParam('avatar'),
+                'phone' => $this->getParam('phone'),
+                'qq' => $this->getParam('qq'),
+                'wechat' => $this->getParam('wechat'),
+                'email' => $this->getParam('email'),
+                'address' => $this->getParam('address'),
+                'remark' => $this->getParam('remark'),
+            ];
+            $id =  Db::table('app_friend')->insert($data, true);
+            $data['id'] = $id;
+            return $this->jsonResponse($data);
+        } catch (Exception $e) {
+            return $this->jsonResponse([], $e->getMessage(), 500);
+        }
     }
 
-    public function delete()
-    {
-        $id = $this->request->param('id');
-        Db::table('app_friend')->where('id', $id)->delete();
-        return $this->jsonResponse();
-    }
-
-    public function query()
-    {
-        $id = $this->request->param('id');
-        $data = Db::table('app_friend')->where('id', $id)->find();
-        return $this->jsonResponse($data);
-    }
-
-
+    /**
+     * 用户档案修改
+     * @return \think\response\Json
+     */
     public function update()
     {
-        $id = $this->request->param('id');
-        $postData = input('post.');
-        Db::table('app_friend')->where('id', $id)->update($postData);
-        return $this->jsonResponse();
+        try {
+            $id = $this->request->param('id');
+            $postData = input('post.');
+            Db::table('app_friend')->where('id', $id)->update($postData);
+            return $this->jsonResponse();
+        } catch (Exception $e) {
+            return $this->jsonResponse([], $e->getMessage(), 500);
+        }
     }
 
+    /**
+     * 用户档案删除
+     * @return \think\response\Json
+     */
+    public function delete()
+    {
+        try {
+            $id = $this->request->param('id');
+            Db::table('app_friend')->where('id', $id)->delete();
+            return $this->jsonResponse();
+        } catch (Exception $e) {
+            return $this->jsonResponse([], $e->getMessage(), 500);
+        }
+    }
+
+    /**
+     * 用户档案查询
+     * @return \think\response\Json
+     */
+    public function query()
+    {
+        try {
+            $id = $this->request->param('id');
+            $data = Db::table('app_friend')->where('id', $id)->find();
+            return $this->jsonResponse($data);
+        } catch (Exception $e) {
+            return $this->jsonResponse([], $e->getMessage(), 500);
+        }
+    }
+
+    /**
+     * 用户档案查询列表
+     * @return \think\response\Json
+     */
     public function queryList()
     {
-        $data = Db::table('app_friend')->select();
-        return $this->jsonResponse($data);
+        try {
+            $data = Db::table('app_friend')->select();
+            return $this->jsonResponse($data);
+        } catch (Exception $e) {
+            return $this->jsonResponse([], $e->getMessage(), 500);
+        }
     }
 }
